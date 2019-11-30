dfc9d2d309cadb3a0f9db28059a433b5946adc1c
Hero's Dad <cody.monta@gmail.com>
2019-11-29 22:34:16 -0600

    committing

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app" class="bg-gray-100 text-center p-6">
+		Hello App 
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'app',
+	components: {},
+}
+</script>
+
+<style src="./assets/css/tailwind.css"></style>
